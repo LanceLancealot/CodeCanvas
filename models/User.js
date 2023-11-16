d39b359f46aa8074069a27eedabe06a91a0ad679
beastrobel <beatrizstrobel@proton.me>
2023-11-15 22:15:44 -0500

    Added sequelize extending model method to User.js

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/connection');
+//Password hashing
+const bcrypt = require('bcrypt');
+
+//Uses sequelize extending model method, checks if password hash matches
+class User extends Model {
+    checkPassword(loginPw) {
+      return bcrypt.compareSync(loginPw, this.password);
+    }
+  }
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primary key: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+        password: {
+            type:DataTypes.STRING,
+            allowNull: false,
+        }
+    }
+);    
