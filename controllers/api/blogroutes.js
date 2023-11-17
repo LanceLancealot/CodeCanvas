552e9edfd6a783e82728fcc7ae0bbdd4a7eccebd
CurtisCircus <cakrieg63@gmail.com>
2023-11-16 20:31:23 -0500

    Some of my code didnt transfer, there shouldn't be any blank files in the api folder (or in controllers at all)

---
+++ controllers/api/blogroutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const { Post, User } = require('../../models');
+
+// Dashboard route - Display posts
+router.get('/dashboard', async (req, res) => {
+  try {
+    // Assuming you have a user ID stored in the session after login
+    const userId = req.user.id; // Adjust this based on your authentication setup
+
+    // Fetch posts for the logged-in user from the database
+    const userPosts = await Post.findAll({
+      where: { userId }, // Adjust this based on your Post model
+      include: [{ model: User, attributes: ['username'] }], // Include the associated user for each post
+    });
+
+    res.render('dashboard', { userPosts });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Other blog-related routes can be added here
+
+module.exports = router;
---
+++ controllers/api/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+// Import and use the sub-routers for specific functionalities
+const blogRoutes = require('./blogRoutes');
+const commentRoutes = require('./commentRoutes');
+const userRoutes = require('./userRoutes');
+
+router.use('/blog', blogRoutes);
+//router.use('/comment', commentRoutes);
+router.use('/user', userRoutes);
+
+module.exports = router;
