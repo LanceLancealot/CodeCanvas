7a7d57e33356823f1ba50ab112e223663746e3b7
LanceLancealot <maxfield.kraus@gmail.com>
2023-11-20 23:00:38 -0500

    Worked On New Post Functionality, Still doesnt work but its closer to being fixed

--- controllers/api/blogroutes.js
+++ controllers/api/blogroutes.js
@@ -6,25 +6,6 @@ const { Post, User } = require('../../models');
 router.get('/dashboard', async (req, res) => {
   try {
     // Assuming you have a user ID stored in the session after login
-    const userId = req.session.user_id; // Adjust this based on your authentication setup
-
-    // Fetch posts for the logged-in user from the database
-    const userPosts = await Post.findAll({
-      where: { userId }, // Adjust this based on your Post model
-      include: [{ model: User, attributes: ['username'] }], // Include the associated user for each post
-    });
-
-    res.render('dashboard', { userPosts });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-// Dashboard route - Display posts
-router.get('/dashboard', async (req, res) => {
-  try {
-    // Assuming you have a user ID stored in the session after login
     const userId = req.user.id; // Adjust this based on your authentication setup
 
     // Fetch posts for the logged-in user from the database
@@ -46,7 +27,7 @@ router.get('/dashboard/new', (req, res) => {
 });
 
 // Route to handle the form submission and create a new blog post
-router.post('/dashboard/new', async (req, res) => {
+router.post('/dashboard/new-post', async (req, res) => {
   try {
     const { title, content } = req.body;
     const userId = req.user.id; // Assuming user information is available in the request
